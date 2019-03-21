958685fcc09ce6d6fbcb6449b6039e9a1ad67327
BIT <BIT@DESKTOP-SU5SFCR>
2019-03-21 15:01:23 +0900

    init

---
+++ src/main/java/com/douzone/mysite/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.douzone.mysite.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+import com.douzone.config.web.MVCConfig;
+import com.douzone.config.web.MessagesConfig;
+import com.douzone.config.web.SecurityConfig;
+
+@Configuration
+@ComponentScan("com.douzone.mysite.controller")
+@Import(value= {MVCConfig.class, SecurityConfig.class, MessagesConfig.class})
+public class WebConfig {
+	
+	
+}
